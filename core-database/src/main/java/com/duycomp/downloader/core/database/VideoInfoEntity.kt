aede30c62382dae93d2f237da8ddb7629440cec0
duychu1 <chuduy3121@gmail.com>
2023-10-17 04:52:12 +0700

    Database setup

---
+++ core-database/src/main/java/com/duycomp/downloader/core/database/VideoInfoEntity.kt
@@ -0,0 +1,22 @@
+package com.duycomp.downloader.core.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+const val DB_NAME = "videos_info_fb"
+
+@Entity(tableName = DB_NAME)
+data class VideoInfoEntity (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id") var id: Int = 0,
+
+    @ColumnInfo(name = "title") val title:String,
+
+    @ColumnInfo(name = "uri") val uri: String,
+
+    @ColumnInfo(name = "duration") val duration: String,
+
+)
+
+
